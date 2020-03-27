2b5b9bdd7a51d26fa40ca5f524c9e84823a27add
DouglasRamosP <43833349+DouglasRamosP@users.noreply.github.com>
2020-03-26 22:46:07 -0300

    Add files via upload

---
+++ ProgC.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ativ03
+{
+    class Carimbo : Interacao
+    {
+        private string cor;
+        private string texto;
+        private int carga;
+
+        /*Construtor*/
+        public Carimbo(string Ptexto)
+        {
+            trocarTexto(Ptexto);
+        }
+
+
+        /*Metodos GET SET*/
+        private string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        private string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        private int Carga
+        {
+            get { return carga; }
+            set { carga = value; }
+        }
+
+        /*Metodos do contrato com interface*/
+        public void usar() 
+        { 
+            if(Carga >= 1)
+            {
+                if(!String.IsNullOrEmpty(Texto))
+                {
+                    if (!String.IsNullOrEmpty(Cor)) 
+                    {
+                        Console.WriteLine("(" + Texto + ") na cor "+Cor);
+                        Carga--;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Impossível Carimbar - Carimbo está sem Cor.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Impossível Carimbar - Carimbo está sem texto.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Impossível Carimbar - Carimbo sem carga!");
+            }
+
+      
+        }
+        public void carregar(string Pcor,int Pcarga)
+        {
+            trocarCor(Pcor);
+
+            if ( (Pcarga < 1 && Pcarga > 10) && ((Carga + Pcarga)> 10))
+            {
+                Console.WriteLine("Carga atual: "+ Carga +", carga deve ser entre 1 e 10");
+            }
+            else
+            {
+                Carga = Pcarga;
+            }
+        }
+        
+        public void trocarCor(string Pcor)
+        {
+            if (Pcor == Cor)
+            {
+                Console.WriteLine("Carimbo já está com esta cor!");
+            }
+            else
+            {
+                Cor = Pcor;
+            }
+        }
+        public void trocarTexto(string Ptexto)
+        {
+            if (Ptexto == this.Texto)
+            {
+                Console.WriteLine("Carimbo já está com este texto.");
+            }
+            else
+            {
+                this.Texto = Ptexto;
+            }
+        }
+    }
+
+    internal class Interacao
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ativ03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carimbo C = new Carimbo("Aprovado");
+            C.carregar("", 2);
+            C.usar();
+
+            C.trocarCor("Azul");
+            C.usar();
+
+
+            C.carregar("Vermelho", 1);
+            C.trocarTexto("Recuperação");
+            C.usar();
+            C.usar();
+
+            C.carregar("Preto", 2);
+            C.trocarTexto("Aprovado");
+            C.usar();
+
+            C.trocarCor("Roxo");
+            C.trocarTexto("");
+            C.usar();
+            System.Console.ReadKey();
+        }
+    }
+}
